10b936b22ea07af67b6413704b2c42dfb631c37d
Vinicius <vini.souza2048@gmail.com>
2025-02-09 22:54:20 -0300

    another fixes with rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,8 +1,5 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
-import sanitizeHtml from 'sanitize-html';
-import MarkdownIt from 'markdown-it';
-const parser = new MarkdownIt();
 
 export async function GET(context) {
   const posts = await getCollection("blog");
@@ -16,11 +13,7 @@ export async function GET(context) {
       pubDate: post.data.pubDate,
       description: post.data.description,
       author: post.data.author,
-      link: `/posts/${post.id}/`,
-      content: sanitizeHtml(parser.render(post.body), {
-        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
-      }),
-      ...post.data,
+      link: `/posts/${post.id}/`
     })),
     customData: `<language>pt-br</language>`,
   })
