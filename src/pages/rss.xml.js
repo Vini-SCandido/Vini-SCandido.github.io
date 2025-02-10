ba6081d2dc839de33cf10384bd73f92c8ab5f52f
Vinicius <vini.souza2048@gmail.com>
2025-02-09 22:19:23 -0300

    add rss feed and first post

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,25 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
+
+export async function GET(context) {
+  const posts = await getCollection("blog");
+  return rss({
+    title: 'Rumor Aleatório',
+    description: 'Um blog de aventuras fantásticas',
+    site: context.site,
+    trailingSlash: false,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.id}/`,
+      content: sanitizeHtml(parser.render(post.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+      }),
+    })),
+    customData: `<language>pt-br</language>`,
+  })
+}
