eeff9cd32cb423c2e4706de29f243054a902a5bf
Vinicius <vini.souza2048@gmail.com>
2025-02-10 10:09:36 -0300

    add rss content

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,9 +1,13 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 
 export async function GET(context) {
   const posts = await getCollection("blog");
   return rss({
+    stylesheet: '/rss/styles.xsl',
     title: 'Rumor Aleatório',
     description: 'Um blog de aventuras fantásticas',
     site: context.site,
@@ -12,7 +16,10 @@ export async function GET(context) {
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
-      link: `/posts/${post.id}/`
+      link: `/posts/${post.id}/`,
+      content: sanitizeHtml(parser.render(post.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+      }),
     })),
     customData: `<language>pt-br</language><atom:link href="https://vini-scandido.github.io/rss.xml" rel="self" type="application/rss+xml" />`,
     xmlns: {
